0c811658526c77d41ee1fb8a407c12d37b75faa9
Pratik <pp3757634@gmail.com>
2024-03-07 21:13:33 +0530

    creating form to add datas to the database

---
+++ vege/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+def receipes(request):
+    if request.method == 'POST':
+        data = request.POST
+        receipe_image = request.FILES.get('receipe_image')
+        print("receipe_image",receipe_image)
+        print(data)
+        Receipe.objects.create(
+            receipe_name = data['receipe_name'],
+            receipe_description = data['receipe_description'],
+            receipe_image = receipe_image,            
+            )
+
+    return render(request,'receipes.html')
